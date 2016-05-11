030e009a2316e06ba64e593cd3208c63baf26052
jens <jho@orgcomnet.de>
2016-05-11 22:52:00 +0200

    * schema.sql added.

--- oauth2/authserver/src/main/resources/schema.sql
+++ oauth2/authserver/src/main/resources/schema.sql
@@ -15,7 +15,7 @@ create table oauth_client_details (
 
 create table oauth_client_token (
   token_id VARCHAR(256),
-  token LONGVARBINARY,
+  token longv,
   authentication_id VARCHAR(256) PRIMARY KEY,
   user_name VARCHAR(256),
   client_id VARCHAR(256)
